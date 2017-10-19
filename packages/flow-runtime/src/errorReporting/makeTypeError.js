9eebcbfe521736b32c1b58385cea5cfe7e87b72a
Andy Edwards <jedwards@fastmail.com>
2017-10-19 12:39:36 -0500

    fix #162

--- packages/flow-runtime/src/errorReporting/makeTypeError.js
+++ packages/flow-runtime/src/errorReporting/makeTypeError.js
@@ -15,13 +15,14 @@ export default function makeTypeError <T> (validation: Validation<T>) {
   const collected = [];
   for (const [path, message, expectedType] of validation.errors) {
     const expected = expectedType ? expectedType.toString() : "*";
-    const actual = context.typeOf(resolvePath(input, path)).toString();
+    const actual = resolvePath(input, path);
+    const actualType = context.typeOf(resolvePath(input, path)).toString();
 
     const field = stringifyPath(validation.path.concat(path));
 
 
     collected.push(
-      `${field} ${message}\n\nExpected: ${expected}\n\nActual: ${actual}\n`
+      `${field} ${message}\n\nExpected: ${expected}\n\nActual: (${actualType}) ${actual}\n`
     );
   }
   if (prefix) {
